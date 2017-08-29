a3e2985b0986dc760a876c113fd8c96d0c979d22
Patrick Pu <pxlxinzhao@gmail.com>
2017-08-29 20:40:04 +0000

    add login module

--- server.js
+++ server.js
@@ -1,26 +1,44 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
 var http = require('http');
 var path = require('path');
-
-var async = require('async');
-// var socketio = require('socket.io');
 var express = require('express');
-
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
 var router = express();
-var server = http.createServer(router);
-// var io = socketio.listen(server);
 
-router.use(express.static(path.resolve(__dirname, 'client')));
+var bodyParser = require('body-parser')
+
+var validSession = [];
+
+
+router.use( bodyParser.json() );       // to support JSON-encoded bodies
+router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+router.use('/', express.static(path.join(__dirname, 'client')));
+
+router.post('/login',　function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  
+  if (username === 'patrick' && password === 'stufie'){
+    validSession.push(req.sessionID);
+    console.log('Add session Id: ', req.sessionID);
+  }
+  res.redirect('/homepage');
+})
+
+router.get('/homepage', function(req, res) {
+    if (validSession.indexOf(req.sessionID) > -1){
+      res.sendFile(path.join(__dirname, 'client/homepage.html'));
+    }else{
+      res.redirect('/');
+    }
+});
+
+router.get('/*', function(req, res) {
+    res.redirect('/');
+});
+
+var server = http.createServer(router);
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
